e5a72112b11c1b4a07e96f34cd2f77d5548ff83f
IgYaHiko <subhrokolay2@gmail.com>
2026-06-24 18:28:02 +0530

    rust backend with axum postgres and email verification

---
+++ src/main.rs
@@ -0,0 +1,71 @@
+mod models;
+mod config;
+mod dtos;
+mod error;
+mod db;
+mod utils;
+mod middleware;
+
+
+use sqlx::postgres::PgPoolOptions;
+use dotenv::dotenv;
+use axum::{Extension, Router, http::{HeaderValue, Method, header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE}}, routing::Route};
+use config::Config;
+use db::DBClient;
+use tower_http::cors::CorsLayer;
+use tracing_subscriber::filter::LevelFilter;
+#[derive(Debug, Clone)]
+pub struct AppState {
+    pub env: Config,
+    pub db_client: DBClient
+
+}
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt()
+    .with_max_level(LevelFilter::DEBUG)
+    .init();
+
+    dotenv().ok();
+    let config = Config::init();
+    let pool = match PgPoolOptions::new()
+                  .max_connections(10)
+                  .connect(&config.database_url)
+                  .await
+        {
+             Ok(pool) => {
+                println!("Connection to the database is successfully done!");
+                pool
+             } Err(err) => {
+                println!("Failed to connect to the database: {:?}", err);
+                std::process::exit(1)
+             }
+        };
+    let cors = CorsLayer::new()
+        .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+        .allow_headers([ACCEPT,AUTHORIZATION,CONTENT_TYPE])
+        .allow_credentials(true)
+        .allow_methods([Method::GET, Method:: POST, Method:: PUT]);
+    let db_client = DBClient::new(pool);
+    let app_state = AppState {
+        env: config.clone(),
+        db_client,
+    };
+
+    let app = Router::new()
+            .layer(Extension(app_state))
+            .layer(cors.clone());
+    println!(
+        "{}",
+        format!("Server is runnning on http://localhost:{}",config.port)
+    );
+
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", &config.port))
+    .await
+    .unwrap();
+
+    axum::serve(listener, app).await.unwrap();
+
+
+       
+}
